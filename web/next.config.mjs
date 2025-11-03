a05a8ac0b91d50df911b76d061b3a3885a19f3d8
jacielarias <126627979+jacielarias@users.noreply.github.com>
2025-11-03 09:33:54 -0500

    Update next.config.mjs

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      protocol: "https",
-      hostname: "safe-actor-a3f0efe503.media.strapiapp.com"
-    ]
-  }
 };
 
 export default nextConfig;
